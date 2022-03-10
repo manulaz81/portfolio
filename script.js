afe168f2b1ee4bda755f385bad2d8427174db411
manulaz81 <78950476+manulaz81@users.noreply.github.com>
2022-03-10 15:42:16 +0100

    equipe

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 $(document).ready(function(){
 	$(window).scroll(function(){
 		if (this.scrollY > 20){
@@ -10,5 +12,30 @@ $(document).ready(function(){
 	$('.menu-btn').click(function(){
 		$('.navbar .menu').toggleClass("active");
 		$('.menu-btn i').toggleClass("active");
+	});
+
+	// own carousel Script
+
+	$('.carousel').owlCarousel({
+		margin: 20,
+		loop: true,
+		autoplayTimeOut: 2000,
+		autoplayHoverPause: true,
+		responsive: {
+			0:{
+				items: 1,
+				nav:false
+			},
+			600:{
+				items: 2,
+				nav: false
+			},
+			1000:{
+				items: 3,
+				nav: false
+			}
+
+
+		}
 	});
 });
